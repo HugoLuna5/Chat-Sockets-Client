ba8905ef468294885b2566947a1a5d53a980db14
HugoLuna <hugo_1199@hotmail.com>
2017-10-09 14:35:13 -0500

    Initial commit

---
+++ app/src/main/java/other/WsConfig.java
@@ -0,0 +1,9 @@
+package other;
+
+/**
+ * Created by HugoLuna on 08/10/17.
+ */
+
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://10.30.195.47:8080/ChatServer/chat?name=";
+}
